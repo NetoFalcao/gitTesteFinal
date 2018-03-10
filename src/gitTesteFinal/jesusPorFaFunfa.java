ca59c96634e325e3e0af6ff684c5630ee77b2326
Falcão <apoioigneto@gmail.com>
2018-03-10 19:15:49 -0300

    funfa porra

--- src/gitTesteFinal/jesusPorFaFunfa.java
+++ src/gitTesteFinal/jesusPorFaFunfa.java
@@ -2,6 +2,14 @@ package gitTesteFinal;
 
 public class jesusPorFaFunfa {
 
+	public static void main(String[] args){
 	//testeeeeeeeeeeeeeeeeeeeeeee
 	//testenovamnete porra
-}
+	// git add funfa carai
+	// agora parece ta pegando vamo ve
+	// namoral n sei realmente bem o que ta acontecendo nessa porra
+	
+	for (int i=0; i < 6; i++){
+		System.out.println(i);
+	}
+}}
